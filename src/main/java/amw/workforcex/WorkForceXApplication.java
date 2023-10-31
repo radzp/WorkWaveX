c63c0b9121de105dab18a987a9edc50e6ae227a1
radzp <anne.paczesna@gmail.com>
2023-10-31 18:49:11 +0100

    init

---
+++ src/main/java/amw/workforcex/WorkForceXApplication.java
@@ -0,0 +1,13 @@
+package amw.workforcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkForceXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkForceXApplication.class, args);
+    }
+
+}
