cbd5f256dd5294774e9641020dce8cc182060d6f
Marcin Wójcik <wojcikmarcin49@gmail.com>
2024-01-13 03:34:57 +0100

    Add login logic

--- src/main/java/amw/workwavex/auth/AuthenticationController.java
+++ src/main/java/amw/workwavex/auth/AuthenticationController.java
@@ -2,6 +2,8 @@ package amw.workwavex.auth;
 
 // class that will allow me to login and register new account and authenticate an existing user
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jdk.jfr.Registered;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +29,23 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate (
-            @RequestBody AuthenticationRequest request
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request,
+            HttpServletResponse response
     ) {
-        return ResponseEntity.ok(service.authenticate(request));
-    }
+        AuthenticationResponse authResponse = service.authenticate(request);
 
+        // Utwórz ciasteczko z tokenem JWT
+        Cookie jwtCookie = new Cookie("jwtToken", authResponse.getToken());
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setPath("/");
+        //jwtCookie.setSecure(true); // Ustaw na true, jeśli używasz HTTPS
+
+        // Dodaj ciasteczko do odpowiedzi
+        response.addCookie(jwtCookie);
+
+        return ResponseEntity.ok(authResponse);
+    }
 
 }
 
--- src/main/java/amw/workwavex/config/JwtAuthenticationFilter.java
+++ src/main/java/amw/workwavex/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package amw.workwavex.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,54 +11,69 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+// Adnotacja @Component mówi Springowi, że ta klasa jest komponentem Springa
+// i powinna być automatycznie zainstancjonowana jako bean
 @Component
-@RequiredArgsConstructor //it will create a constructor with all required fields for final fields
-
+// Adnotacja @RequiredArgsConstructor generuje konstruktor z wymaganymi argumentami
+// dla wszystkich finalnych pól (w tym przypadku JwtService i UserDetailsService)
+@RequiredArgsConstructor
+// Klasa JwtAuthenticationFilter rozszerza OncePerRequestFilter, co oznacza, że jest to filtr,
+// który jest wywoływany raz na żądanie
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    // Wstrzyknięcie zależności JwtService i UserDetailsService
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService; // Spring Security interface to retrieve user-related data
+    private final UserDetailsService userDetailsService;
 
+    // Metoda doFilterInternal jest wywoływana dla każdego żądania
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization"); //get the header from the request, that is the header that contains the JWT token or bearer token
-        final String jwt;
-        final String userEmail;
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) { //if the header is null or does not start with Bearer, then we will not do anything
-            filterChain.doFilter(request, response);
-            return;
+        // Pobierz ciasteczko z tokenem JWT
+        Cookie jwtCookie = Arrays.stream(request.getCookies())
+                .filter(cookie -> "jwtToken".equals(cookie.getName()))
+                .findFirst()
+                .orElse(null);
+
+
+//        // Jeśli ciasteczko "jwtToken" nie istnieje i użytkownik nie jest już na stronie logowania, przekieruj na stronę logowania
+//        if (jwtCookie == null && !request.getRequestURI().equals("/")) {
+//            response.sendRedirect("/");
+//            return;
+//        }
+
+        String jwt = null;
+        String userEmail = null;
+
+        // Jeśli ciasteczko "jwtToken" istnieje, kod odczytuje token JWT i nazwę użytkownika z tokena
+        if (jwtCookie != null) {
+            jwt = jwtCookie.getValue();
+            userEmail = jwtService.extractUsername(jwt);
         }
-        jwt = authHeader.substring(7); //if the header is not null and starts with Bearer, then we will extract the JWT token from the header
-        userEmail = jwtService.extractUsername(jwt); //we will extract the username from the JWT token
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) //when the authentication is null that means that the user is not yet authenticated, the user is not connected yet
-        {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); //we will load the user details from the database using the username (email)
-            if(jwtService.isTokenValid(jwt, userDetails)) //we will check if the JWT token is valid
-            {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken( //we will create the authentication token
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken); //we will set the authentication token in the security context
+
+        // Jeśli nazwa użytkownika istnieje i użytkownik nie jest jeszcze uwierzytelniony,
+        // kod ładuje szczegóły użytkownika, weryfikuje token JWT i ustawia kontekst bezpieczeństwa
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
 
-        filterChain.doFilter(request, response); //always calling the filterChain.doFilter(request, response) method to continue the filter chain
-
+        // Na końcu, filtr przekazuje żądanie dalej do następnego filtra w łańcuchu filtrów
+        filterChain.doFilter(request, response);
     }
 }
