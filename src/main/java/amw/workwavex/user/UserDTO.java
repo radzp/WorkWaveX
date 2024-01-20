12a1ea1c852745af04c210283e1b712714e8376f
radzp <anne.paczesna@gmail.com>
2024-01-20 17:09:34 +0100

    UserDTO update

--- src/main/java/amw/workwavex/user/UserDTO.java
+++ src/main/java/amw/workwavex/user/UserDTO.java
@@ -1,10 +1,10 @@
 package amw.workwavex.user;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class UserDTO {
     private Integer id;
     private String firstName;
