df91ebc5ff7269804775c5871e986dac37a9f866
Marcin Wójcik <wojcikmarcin49@gmail.com>
2024-01-20 17:15:11 +0100

    User Controller update

--- src/main/java/amw/workwavex/user/UserController.java
+++ src/main/java/amw/workwavex/user/UserController.java
@@ -1,5 +1,22 @@
 package amw.workwavex.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/user-controller")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUserDTOs());
+    }
 
 }
